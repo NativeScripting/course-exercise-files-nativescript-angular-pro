c5ac6e6c925fc15b171af52f7ef32a386a332f51
Alexander Ziskind <alex@nuvious.com>
2017-11-09 15:35:21 -0500

    Added rpstrackertnsng project

---
+++ Chapter1/rpstrackertnsng/app/app.routing.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { NativeScriptRouterModule } from "nativescript-angular/router";
+import { Routes } from "@angular/router";
+
+
+const routes: Routes = [
+    { path: "", redirectTo: "/items", pathMatch: "full" }
+];
+
+@NgModule({
+    imports: [NativeScriptRouterModule.forRoot(routes)],
+    exports: [NativeScriptRouterModule]
+})
+export class AppRoutingModule { }
