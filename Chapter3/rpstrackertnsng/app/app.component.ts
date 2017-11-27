d3d9af10295f8cddd4af8b518792db32bb7a60cf
Alexander Ziskind <alex@nuvious.com>
2017-11-26 20:14:37 -0500

    Chapter 3 lesson 4 - observable store pattern - simple store example

--- Chapter3/rpstrackertnsng/app/app.component.ts
+++ Chapter3/rpstrackertnsng/app/app.component.ts
@@ -3,16 +3,20 @@ import { Page } from 'ui/page';
 import { device } from 'platform';
 import { TranslateService } from '@ngx-translate/core';
 import { LoggerService } from './core/services';
+import { Store } from './core/state/app-store';
+import { PtItem } from './core/models/domain';
 
 @Component({
     selector: 'ns-app',
     templateUrl: 'app.component.html',
 })
 export class AppComponent {
-    public myAppText = 'Hello';
+
+    public items$ = this.store.select<PtItem[]>('backlogItems');
 
     constructor(
         private page: Page,
+        private store: Store,
         private translateService: TranslateService,
         private loggerService: LoggerService
     ) {
@@ -20,7 +24,23 @@ export class AppComponent {
         page.actionBarHidden = true;
         page.backgroundSpanUnderStatusBar = true;
 
-        this.myAppText = device.language;
+        const fakeBacklogItems: PtItem[] = [
+            {
+                id: 1,
+                title: 'item 1',
+                dateCreated: new Date(),
+                dateModified: new Date()
+            },
+            {
+                id: 2,
+                title: 'item 2',
+                dateCreated: new Date(),
+                dateModified: new Date()
+            }
+        ];
+
+        this.store.set('backlogItems', fakeBacklogItems);
+
         translateService.setDefaultLang('en');
         translateService.use(device.language);
     }
