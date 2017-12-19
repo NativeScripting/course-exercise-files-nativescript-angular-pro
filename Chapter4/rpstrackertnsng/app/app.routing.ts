a48df965abc0afe9e19cd9ecb632c9a95b549136
Alexander Ziskind <alex@nuvious.com>
2017-12-19 15:52:46 -0500

    Chapter 4 Lesson 3 - declarative routing options

--- Chapter4/rpstrackertnsng/app/app.routing.ts
+++ Chapter4/rpstrackertnsng/app/app.routing.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { NativeScriptRouterModule } from 'nativescript-angular/router';
 import { Routes } from '@angular/router';
+import { AuthPageComponent } from './modules/auth/pages';
+import { BacklogPageComponent } from './modules/backlog/pages';
 
 
 const routes: Routes = [
-    { path: '', redirectTo: '/', pathMatch: 'full' }
+    { path: '', redirectTo: '/auth', pathMatch: 'full' },
+    { path: 'auth', component: AuthPageComponent },
+    { path: 'backlog', component: BacklogPageComponent }
 ];
 
 @NgModule({
